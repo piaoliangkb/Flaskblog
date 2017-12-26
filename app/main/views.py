5ba81b5828c94d41fef9073429ff35ea885ea275
piaoliangkb <418508556@qq.com>
2017-12-26 18:52:51 +0800

    init git with gitignore finished

---
+++ app/main/views.py
@@ -0,0 +1,27 @@
+from flask import render_template,session,redirect,url_for
+from .. import db
+from . import main
+from ..models import User
+from .forms import NameForm
+from datetime import datetime
+
+
+@main.route('/',methods=['GET','POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user=User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known']=False
+        else:
+            session['known']=True
+        session['name']=form.name.data
+        return redirect(url_for('main.index'))
+    return render_template('index.html', form=form,
+                           name=session.get('name'),
+                           known=session.get('known',False),
+                           current_time=datetime.utcnow())
