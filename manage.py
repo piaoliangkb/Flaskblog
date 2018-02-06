c5d066bd00863aa30efc08e1d9ec66ddef2f72f3
piaoliangkb <418508556@qq.com>
2018-02-06 17:36:29 +0800

    finish decorators.py

--- manage.py
+++ manage.py
@@ -1,11 +1,6 @@
 from app import create_app
 from flask_script import Manager
 import os
-from app.models import Role,User
-
-from app.models import User
-from app import db
-
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
 
