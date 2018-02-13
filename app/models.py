3b2bc812107d8c427f8163993a7487e7cf608e19
piaoliangkb <418508556@qq.com>
2018-02-14 00:53:21 +0800

    html shows after markdowned view

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from . import db, login_manager
 from flask import current_app, request
 from datetime import datetime
 import hashlib
+from markdown import markdown
+import bleach
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -12,6 +14,16 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)#保存转换后markdown文章的html代码
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
 
     @staticmethod
     def generate_fake(count=100):
@@ -27,6 +39,7 @@ class Post(db.Model):
                      author=u)
         db.session.add(p)
         db.session.commit()
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 class PERMISSION:
     FOLLOW = 1
