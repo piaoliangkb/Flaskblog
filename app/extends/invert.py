94ba599688be208764fab004e3ccad8991219b90
piaoliangkb <418508556@qq.com>
2018-04-07 00:21:34 +0800

    fix somebugs

--- app/extends/invert.py
+++ app/extends/invert.py
@@ -6,12 +6,12 @@ basepath = Config.BOOLEANSEARCH_PATH
 class InvertedFile():
     @staticmethod
     def BuildDocumentIndex():
-        if os.path.exists(basepath + "\documentindex.txt"):
+        if os.path.exists(basepath + "/documentindex.txt"):
             return
         #如果已经存在文件索引文件，则跳过该函数
 
         index = 1
-        with open(basepath + "\documentindex.txt", "w") as f:
+        with open(basepath + "/documentindex.txt", "w") as f:
             for filename in os.listdir(basepath):
                 if filename != "documentindex.txt":
                     f.write(str(index) + "\t" + os.path.join(basepath, filename) + "\n")
@@ -19,12 +19,12 @@ class InvertedFile():
 
     @staticmethod
     def BuildWordIndex():
-        if os.path.exists(basepath + "\wordindex.txt"):
+        if os.path.exists(basepath + "/wordindex.txt"):
             return
         #如果已经存在单词索引文件，则跳过该函数
 
         WordDir = {} # 建立字典保存单词和文章编号列表的对应关系
-        with open(basepath + "\documentindex.txt") as f:
+        with open(basepath + "/documentindex.txt") as f:
             for line in f:
                 fileindex = line.split('\t')[0]
                 filepath = line.split('\t')[1].replace('\n', '')
@@ -39,7 +39,7 @@ class InvertedFile():
                             WordDir[item].append(fileindex)
                         else:
                             WordDir[item] = [fileindex]
-        with open(basepath + "\wordindex.txt", "w") as f:
+        with open(basepath + "/wordindex.txt", "w") as f:
             for item in WordDir.keys():
                 f.write(item + '\t')
                 for i in WordDir[item]:
